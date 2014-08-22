3892e1c3e2dcd115d391fe3ed9693f78c5c012e2
shogo <shogo@studiofly.net>
2014-08-22 16:46:56 +0900

    Brewfile を廃止して brew.sh を追加した

---
+++ brew.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+brew_install(){
+  brew install $1 || brew upgrade $1 || true
+}
+
+cask_install(){
+  brew cask install $1 || true
+}
+
+brew update
+brew cleanup
+
+brew_install caskroom/cask/brew-cask
+
+brew tap caskroom/homebrew-versions
+
+brew_install ffmpeg
+brew_install git
+brew_install giter8
+brew_install maven
+brew_install mobile-shell
+brew_install nkf
+brew_install phantomjs
+brew_install rename
+brew_install sbt
+brew_install scala
+brew_install tree
+brew_install typesafe-activator
+brew_install wget
+
+cask_install alfred
+cask_install atom
+cask_install dash
+cask_install dropbox
+cask_install firefox-ja
+cask_install google-chrome
+cask_install intellij-idea-ce
+cask_install iterm2
+cask_install java7
+cask_install libreoffice
+cask_install phpstorm
+cask_install sequel-pro
+cask_install slack
+cask_install sourcetree
+cask_install vagrant
+cask_install virtualbox
+
+brew cleanup
+brew cask cleanup
