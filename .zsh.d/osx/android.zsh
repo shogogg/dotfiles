63812427813d3bbae19c92607ac0b88111b190e5
shogogg <shogo@studiofly.net>
2015-07-29 18:09:54 +0900

    OS X向け android.zsh を Homebrew 対応に書き換える

--- .zsh.d/osx/android.zsh
+++ .zsh.d/osx/android.zsh
@@ -1,8 +1,3 @@
-export ANDROID_HOME=/opt/local/android-sdk-macosx
+export ANDROID_HOME=/usr/local/opt/android-sdk
 export ANDROID_SDK_HOME="$ANDROID_HOME"
 export ANDROID_SDK_ROOT="$ANDROID_HOME"
-path=(
-  ${ANDROID_HOME}/tools(N-/)
-  ${ANDROID_HOME}/platform-tools(N-/)
-  $path
-)
