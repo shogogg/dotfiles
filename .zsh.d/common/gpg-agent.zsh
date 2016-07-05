c780c89023b6423eac6830f2f4f1ac96f10df9d4
shogogg <shogo@studiofly.net>
2016-07-05 17:39:37 +0900

    GPGに関する設定を追加

---
+++ .zsh.d/common/gpg-agent.zsh
@@ -0,0 +1,4 @@
+pgrep -q gpg-agent || eval $(gpg-agent --daemon --write-env-file ${HOME}/.gpg-agent-info)
+[ -f ${HOME}/.gpg-agent-info ] && source ${HOME}/.gpg-agent-info
+export GPG_AGENT_INFO
+export GPG_TTY=`tty`
