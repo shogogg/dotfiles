72ac049effdc39e947bc7821f5809a5139c61a7a
shogogg <shogo@studiofly.net>
2016-07-22 22:04:00 +0900

    Use marzocchi/zsh-notify, instead of shogogg/zsh-notify

--- .zsh.d/antigen.zsh
+++ .zsh.d/antigen.zsh
@@ -7,4 +7,4 @@ antigen bundle b4b4r07/enhancd
 zstyle ':notify:*' command-complete-timeout 5
 zstyle ':notify:*' error-sound Glass
 zstyle ':notify:*' success-sound default
-antigen bundle shogogg/zsh-notify
+antigen bundle marzocchi/zsh-notify
