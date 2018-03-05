68d90e6da5268eb53d4e64ae820ea6198813e4a3
shogogg <shogo@studiofly.net>
2018-03-05 19:13:02 +0900

    :zap: gpg-agent の仕様変更に対応

--- .zsh.d/common/gpg-agent.zsh
+++ .zsh.d/common/gpg-agent.zsh
@@ -1,4 +1,7 @@
-pgrep -q gpg-agent || eval $(gpg-agent --daemon --write-env-file ${HOME}/.gpg-agent-info)
-[ -f ${HOME}/.gpg-agent-info ] && source ${HOME}/.gpg-agent-info
-export GPG_AGENT_INFO
-export GPG_TTY=`tty`
+# Since v2.1 GnuPG have changed the method a daemon starts. They are all started
+# on demand now. For more information see:
+#   https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart
+gpgconf --launch gpg-agent
+
+export GPG_TTY=$(/usr/bin/tty)
+export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
