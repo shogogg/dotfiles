2421ddddc95b1ad3afdd51801a39be207b88d8fa
shogogg <shogo@studiofly.net>
2018-05-15 18:45:45 +0900

    :zap: キーバインドを見直し

--- .zsh.d/keybind.zsh
+++ .zsh.d/keybind.zsh
@@ -1,10 +1,16 @@
 # Reset
 bindkey -d
 
-# Cursor
+# Bind
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
-
-# History
-bindkey "^P" history-beginning-search-backward-end
+bindkey "^L" clear-screen
 bindkey "^N" history-beginning-search-forward-end
+bindkey "^P" history-beginning-search-backward-end
+# bindkey "^R" history-fzy
+bindkey "^U" vi-kill-line
+bindkey "^W" vi-backward-kill-word
+
+bindkey '\e\C-?' vi-backward-kill-word  # ⌥ + ⌫
+bindkey '\e\e[C' vi-forward-word        # ⌥ + →
+bindkey '\e\e[D' vi-backward-word       # ⌥ + ←
