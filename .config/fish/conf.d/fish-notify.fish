e29bd14cb015fff301a6a8d75356326c81b14d84
shogogg <shogo.kawase@eustylelab.co.jp>
2021-02-16 14:08:02 +0900

    :sparkles: Switch sound on notification when command failed

--- .config/fish/conf.d/fish-notify.fish
+++ .config/fish/conf.d/fish-notify.fish
@@ -1,17 +1,17 @@
 function fish-notify --on-event fish_prompt
-  set _display_status $status
+  set -l _display_status $status
   if test $CMD_DURATION
-    set secs (math "$CMD_DURATION / 1000")
+    set -l secs (math "$CMD_DURATION / 1000")
     if test $secs -ge 5
       terminal-notifier \
         -title $history[1] \
-        -subtitle (if test $_display_status -eq 0; echo 'Success'; else; echo 'Failure'; end) \
-        -message "Took $secs seconds" \
+        -subtitle (if test $_display_status -eq 0; echo 'SUCCESS'; else; echo 'FAILURE'; end) \
+        -message "$secs seconds" \
         -group "fish-notify" \
         -remove "fish-notify" \
         -activate "co.zeit.hyper" \
         -sender "co.zeit.hyper" \
-        -sound Ping \
+        -sound (if test $_display_status -eq 0; echo 'Ping'; else; echo 'Glass'; end) \
         > /dev/null 2> /dev/null
     end
   end
