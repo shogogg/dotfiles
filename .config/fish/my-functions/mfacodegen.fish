606c01b9ad79d683f11d693ae1a59f70551f46da
shogogg <shogo.kawase@eustylelab.co.jp>
2021-02-16 14:08:26 +0900

    :sparkles: Add subcommand to mfacodegen: edit

--- .config/fish/my-functions/mfacodegen.fish
+++ .config/fish/my-functions/mfacodegen.fish
@@ -1,14 +1,34 @@
 function __mfacodegen_load
-  set -l dat ~/.mfa/accounts.dat
-  set -l key ~/.mfa/mfa.key
+  set -l dat $argv[1]
+  set -l key $argv[2]
   sudo openssl smime -decrypt -binary -inform PEM -in $dat -inkey $key 2> /dev/null
 end
 
-function mfacodegen
+function __mfacodegen_edit
+  set -l dat $argv[1]
+  set -l key $argv[2]
+  set -l crt ~/.mfa/mfa.crt
+  set -l outfile (sudo mktemp)
+
+  # Remove outfile before exit
+  function __mfacodegen_edit_exit --on-job-exit %self --inherit-variable outfile
+    functions -e __mfacodegen_edit_exit
+    sudo rm -rf $outfile
+  end
+
+  sudo openssl smime -decrypt -binary -inform PEM -in $dat -inkey $key -out $outfile 2> /dev/null
+  sudo cat $outfile
+  sudo vi $outfile
+  sudo openssl smime -encrypt -aes256 -in $outfile -out $dat -binary -outform PEM $crt
+end
+
+function __mfacodegen_read
   # Read accounts
   set -l record
   set -l records
-  for record in (__mfacodegen_load)
+  set -l dat $argv[1]
+  set -l key $argv[2]
+  for record in (__mfacodegen_load $dat $key)
     set --append records $record
   end
   if test $status -ne 0
@@ -37,4 +57,15 @@ function mfacodegen
 
   # Copy totp token to clipboard
   type -q pbcopy && echo -n "$token" | pbcopy
+end
+
+function mfacodegen
+  set -l dat ~/.mfa/accounts.dat
+  set -l key ~/.mfa/mfa.key
+  switch $argv[1]
+    case edit
+      __mfacodegen_edit $dat $key
+    case '*'
+      __mfacodegen_read $dat $key
+  end
 end
